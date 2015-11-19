ce2142a0cafd510f3deab06d80378b098da8ddc1
Semen Kamenetskiy <skamenetskiy@live.com>
2015-11-19 11:33:36 +0300

    fixes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var mailer = require('nodemailer');
 plugins.notifications.register('mail', function() {
     console.log(arguments);
 });
-plugins.alerts.register('mail', function(config, handler, method, data) {
+plugins.alarms.register('mail', function(config, handler, method, data) {
     if (undefined !== mail && config.to) {
         mail.send(config.to, 'ClusterControl alert', JSON.stringify(data));
     }
